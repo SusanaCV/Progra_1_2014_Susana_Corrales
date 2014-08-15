11361124347b71262b5e270c83e85a7b23154dd6
SusanaCV <susanacorrales03@gmail.com>
2014-08-15 15:48:41 -0600

    Class 24

---
+++ code/Class24/src/class24/Class24.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class24;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Class24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        frmGraficos oGraficos = new frmGraficos();
+        oGraficos.setVisible(true);
+        frmLiga oLiga= new frmLiga();
+        oLiga.setVisible(true);
+        frmGrafico oGrafico= new frmGrafico();
+        oGrafico.setVisible(true);
+        
+       
+    }
+}
